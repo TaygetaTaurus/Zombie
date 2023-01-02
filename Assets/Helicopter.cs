9fd4ff479165baed3703e576e692bda45c460cce
unknown <fentoru@gmail.com>
2023-01-02 13:31:38 +0500

    v2.48 added input buttons for zoom and call the helicopter

---
+++ Assets/Helicopter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Helicopter : MonoBehaviour {
+
+	public AudioClip callSound;
+
+	private bool called = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Call heli") && !called) {
+			called = true;
+			callSound.
+		}
+	}
+}
