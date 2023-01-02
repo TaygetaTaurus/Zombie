2a0b4c491034866a421066eaf0e7282f73fde7a7
unknown <fentoru@gmail.com>
2023-01-02 15:53:10 +0500

    v2.52 added day-night cycle

---
+++ Assets/Scripts/Daycycle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daycycle : MonoBehaviour {
+
+	[Tooltip("Количество минут в секунду")]
+	public float minutesPerSecond = 60;
+
+	private Quaternion startRotation;
+	// Use this for initialization
+	void Start () {
+		startRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float angleThisFrame = Time.deltaTime/360 * minutesPerSecond;
+		transform.RotateAround (transform.position, Vector3.forward, angleThisFrame);
+	}
+}
--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -1,24 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 
 public class Helicopter : MonoBehaviour {
 
 	public AudioClip callSound;
 
 	private bool called = false;
+	private AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
-		
+		audioSource = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButton ("Call heli") && !called) {
 			called = true;
-			callSound.
+			audioSource.clip = callSound;
+			audioSource.Play ();
 		}
 	}
 }
